ac80e3ed9f8eaefa8941c1b571dc3b6cf342e311
Neon Kamuy <neonkamuy@gmail.com>
2019-11-06 05:09:40 +0400

    Ваша ветка обновлена в соответствии с «origin/master».

--- html/js/carousel.js
+++ html/js/carousel.js
@@ -118,6 +118,7 @@ var carousel = {
       var temp_offset = carousel.offset.first;
       for(i=0; i!=carousel.length; ++i){
         carousel.elems[i].style.left = temp_offset + 'px';
+        carousel.elems[i].style.display = 'block';
         temp_offset += carousel.width;
       }
     }
